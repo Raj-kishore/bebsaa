c7cbb65e9217ec64ee90fa5edf71c1c58d86b4c5
Raaz Babbar <quickroutes@gmail.com>
2023-04-17 02:42:24 +0530

    feat(location): screen to choose location

--- FrontEnd/ReactNative/MyApp/App.tsx
+++ FrontEnd/ReactNative/MyApp/App.tsx
@@ -1,214 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { type PropsWithChildren, useState, useRef, Fragment } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  TouchableOpacity,
   View,
-  Button,
 } from 'react-native';
-import PhoneInput from "react-native-phone-number-input";
+import { RadioButton, Button , Text} from 'react-native-paper';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import AtriInput from './components/Atri/Input';
 import AtriButton from './components/Atri/Button';
 
+import styles from './styles/global';
 
-export const Splash: React.FC = () => {
-  const [value, setValue] = useState("");
-  const [formattedValue, setFormattedValue] = useState("");
-  const [valid, setValid] = useState(false);
-  const [showMessage, setShowMessage] = useState(false);
-  const phoneInput = useRef<PhoneInput>(null);
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <SafeAreaView style={styles.wrapper}>
-          {/* {showMessage && (
-            <View style={styles.message}>
-              <Text>Value : {value}</Text>
-              <Text>Formatted Value : {formattedValue}</Text>
-              <Text>Valid : {valid ? "true" : "false"}</Text>
-            </View>
-          )} */}
-
-
-          <View style={styles.message}>
-            <Text>Bebsaa</Text>
-          </View>
-
-          <PhoneInput
-            ref={phoneInput}
-            defaultValue={value}
-            defaultCode="DM"
-            layout="first"
-            onChangeText={(text) => {
-              setValue(text);
-            }}
-            onChangeFormattedText={(text) => {
-              setFormattedValue(text);
-            }}
-            withDarkTheme
-            withShadow
-            autoFocus
-          />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              const checkValid = phoneInput.current?.isValidNumber(value);
-              setShowMessage(true);
-              setValid(checkValid ? checkValid : false);
-            }}
-          >
-            <Text style={styles.loginBtn}>Let's Begin</Text>
-          </TouchableOpacity>
-        </SafeAreaView>
-      </View>
-    </>
-  );
-};
-
-export const LoginForm: React.FC = () => {
-  const [value, setValue] = useState("");
-  const [formattedValue, setFormattedValue] = useState("");
-  const [valid, setValid] = useState(false);
-  const [showMessage, setShowMessage] = useState(false);
-  const phoneInput = useRef<PhoneInput>(null);
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <SafeAreaView style={styles.wrapper}>
-          {/* {showMessage && (
-            <View style={styles.message}>
-              <Text>Value : {value}</Text>
-              <Text>Formatted Value : {formattedValue}</Text>
-              <Text>Valid : {valid ? "true" : "false"}</Text>
-            </View>
-          )} */}
-
-          <Fragment>
-            <View style={styles.message}>
-              <Text>Bebsaa</Text>
-            </View>
-
-
-            <AtriInput placeHolder="email" />
-            <AtriInput placeHolder="password" />
-            <AtriButton />
-          </Fragment>
-
-
-        </SafeAreaView>
-      </View>
-    </>
-  );
-};
 
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [value, setValue] = React.useState('4');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          {/* <Splash /> */}
-
-          <LoginForm />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <View>
+      <Text variant="displaySmall">Choose your location</Text>
+        <RadioButton.Group onValueChange={value => setValue(value)} value={value}>
+          <RadioButton.Item label="Mumbai" value="1" />
+          <RadioButton.Item label="Bengaluru" value="2" />
+          <RadioButton.Item label="Delhi/NCR" value="3" />
+          <RadioButton.Item label="Hyderabad" value="4" />
+          <RadioButton.Item label="Ahmedabad" value="5" />
+          <RadioButton.Item label="Chennai" value="6" />
+          <RadioButton.Item label="Kolkata" value="7" />
+          <RadioButton.Item label="Surat" value="8" />
+          <RadioButton.Item label="Pune" value="9" />
+          <RadioButton.Item label="Goa" value="10" />
+          <RadioButton.Item label="Chandigarh" value="11" />
+        </RadioButton.Group>
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Next
+        </Button>
+      </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.lighter,
-  },
-  wrapper: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  message: {
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 20,
-    marginBottom: 20,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-  button: {
-    marginTop: 20,
-    height: 50,
-    width: 300,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2ecc71',
-    borderRadius: 10,
-    shadowColor: 'rgba(0,0,0,0.4)',
-    shadowOffset: {
-      width: 1,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-    elevation: 10,
-  },
-  loginBtn: {
-    color: "#ffffff"
-  }
-});
+
 
 export default App;
---
+++ FrontEnd/ReactNative/MyApp/styles/global.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+//Global styles
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    }
+  });
+  
+
+export default styles;
