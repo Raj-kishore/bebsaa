e81e516f8c7236dfcaccf5fd340205cac5e346c5
Raaz Babbar <quickroutes@gmail.com>
2023-05-23 17:41:36 +0530

    chore(bat): updated flag

--- FrontEnd/JS/BebsaaDelivery/app-starter.bat
+++ FrontEnd/JS/BebsaaDelivery/app-starter.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM React native app starter commnads
 ECHO Starting Metro on a new window.
-start cmd.exe /k npx react-native start --reset-cache
+start cmd.exe /k npx react-native start -- --reset-cache
 ECHO Metro is running.
 ECHO Starting Application on a new window.
 start cmd.exe /k  npx react-native run-android
